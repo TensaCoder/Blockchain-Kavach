5a2c4daf2df1728e555aaa35244812ef4757da56
TensaCoder <herschel.menezes00@gmail.com>
2023-04-08 18:59:18 +0530

    Navbar Component edited

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,36 +3,19 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#home">
-                    Navbar
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="d-flex justify-content-end">
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#bitcoin">
-                                    Bitcoin
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#ethereum">
-                                    Ethereum
-                                </a>
-                            </li>
-                        </ul>
+            <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="#">Bitcoin</a>
+                            <a class="nav-link" href="#">Ethereum</a>
+                            <a class="nav-link" href="#">Others</a>
+                            {/* <a class="nav-link disabled">Disabled</a> */}
+                        </div>
                     </div>
                 </div>
             </nav>
